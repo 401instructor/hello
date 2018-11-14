9c644b1805a84335c735a1bf5f68ab84768e3271
villa <villa@DESKTOP-J55HSBA>
2018-11-14 15:24:52 -0800

    Added unit test

---
+++ test/hello/NewEmptyJUnitTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author villa
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        System.out.println("set up");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void found() {
+        List<String> names = Arrays.asList("Tim", "Polly", "Peg");
+        assertEquals(1, Hello.linearSearch(names, (name) -> name.contains("ll")));
+    }
+
+}
